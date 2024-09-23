1976f9ef2db3a62894b6cb83769952121728dbb1
ziadelnady <ziadelnady555@gmail.com>
2024-09-24 00:33:59 +0300

    project setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from 'next';
+import { Calistoga, Inter } from 'next/font/google';
+import { twMerge } from 'tailwind-merge';
+import './globals.css';
+
+const inter = Inter({ subsets: ['latin'], variable: '--font-sans' });
+const calistoga = Calistoga({ subsets: ['latin'], variable: '--font-serif', weight: ['400'] });
+
+export const metadata: Metadata = {
+	title: 'My Portfolio',
+	description: 'Created with the help of Frontend Tribe'
+};
+
+export default function RootLayout({
+	children
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<html lang="en">
+			<body className={twMerge(inter.variable, calistoga.variable, 'bg-gray-900 text-white antialiased font-sans')}>{children}</body>
+		</html>
+	);
+}
