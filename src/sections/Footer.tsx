eac947c8735ddc860169a2a46859c830a5e4cb78
ziadelnady <ziadelnady555@gmail.com>
2024-09-26 01:24:37 +0300

    added FooterSection

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,3 +1,41 @@
+import ArrowUpRight from '@/assets/icons/arrow-up-right.svg';
+
+const footerLinks = [
+	{
+		title: 'Youtube',
+		href: '#'
+	},
+	{
+		title: 'X',
+		href: '#'
+	},
+	{
+		title: 'Instagram',
+		href: '#'
+	},
+	{
+		title: 'LinkedIn',
+		href: '#'
+	}
+];
+
 export const Footer = () => {
-  return <div>Footer Section</div>;
+	return (
+		<footer className="relative -z-10 overflow-x-clip">
+			<div className="absolute h-[400px] w-[1600px] bottom-0 left-1/2 -translate-x-1/2 bg-emerald-300/30 [mask-image:radial-gradient(50%_50%_at_bottom_center,black,transparent)] -z-10" />
+			<div className="container">
+				<div className="border-t border-white/15 py-6 text-sm flex flex-col md:flex-row items-center justify-between gap-8">
+					<div className="text-white/40">&copy; 2024. All rights reserved.</div>
+					<nav className="flex flex-col md:flex-row items-center gap-8">
+						{footerLinks.map((link) => (
+							<a key={link.title} href={link.href} className="inline-flex items-center gap-1.5">
+								<span className="font-semibold">{link.title}</span>
+								<ArrowUpRight className="size-4" />
+							</a>
+						))}
+					</nav>
+				</div>
+			</div>
+		</footer>
+	);
 };
