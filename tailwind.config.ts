c346a9acbd9800e002d1234083f494902168cec2
ziadelnady <ziadelnady555@gmail.com>
2024-09-30 08:27:26 +0300

    animated TapeSection

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -26,10 +26,19 @@ const config: Config = {
 						transform: 'scale(3)',
 						opacity: '0'
 					}
+				},
+				'move-left': {
+					'0%': {
+						transform: 'translateX(0%)'
+					},
+					'100%': {
+						transform: 'translateX(-50%)'
+					}
 				}
 			},
 			animation: {
-				'ping-large': 'ping-large 1s ease-in-out infinite'
+				'ping-large': 'ping-large 1s ease-in-out infinite',
+				'move-left': 'move-left 1s linear infinite'
 			}
 		}
 	},
