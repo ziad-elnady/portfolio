cae9b77ff76b6e5c784576420b2780ae1a41bac4
ziadelnady <ziadelnady555@gmail.com>
2024-09-30 09:27:43 +0300

    fixed z-index in Hero / Footer Sections

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -21,7 +21,7 @@ const footerLinks = [
 
 export const Footer = () => {
 	return (
-		<footer className="relative -z-10 overflow-x-clip">
+		<footer className="relative z-0 overflow-x-clip">
 			<div className="absolute h-[400px] w-[1600px] bottom-0 left-1/2 -translate-x-1/2 bg-emerald-300/30 [mask-image:radial-gradient(50%_50%_at_bottom_center,black,transparent)] -z-10" />
 			<div className="container">
 				<div className="border-t border-white/15 py-6 text-sm flex flex-col md:flex-row items-center justify-between gap-8">
